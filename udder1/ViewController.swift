23139c86fc1b9bb8c8fc8793ef38459821ccd5e9
ajaysolution <ajay.vandra.sa@gmail.com>
2020-01-27 14:52:54 +0530

    Initial Commit

---
+++ udder1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udder1
+//
+//  Created by Ajay Vandra on 1/27/20.
+//  Copyright © 2020 Ajay Vandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
